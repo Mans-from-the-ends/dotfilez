1d3b57ca208b1887ff784bde515e4e77f9d69fb6
Mans-from-the-ends <TheSwedichFrog@gmail.com>
2021-05-11 19:37:04 +0200

    Test

---
+++ Hyper.js
@@ -0,0 +1,20 @@
+module.exports = {
+  	plugins: [
+        "hyper-font-ligatures",
+        "hyper-one-dark",
+        "hyper-search",
+        "hyper-pane",
+        "hypercwd",
+        "hyper-active-tab",
+        "hyperpower"
+    ],	
+  	config: {
+	shell: 'C:\\Windows\\System32\\bash.exe',
+	activeTab: '🚀',
+	webGLRenderer: false,
+	fontSize: 16,
+        fontFamily: '"SpaceMono NF", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+	  // rest of the config
+  }
+  // rest of the file
+}
---
+++ zsh/aliases.zsh
@@ -0,0 +1,19 @@
+alias ls='exa --icons'
+alias zshreload='source ~/.zshrc'             # reload ZSH
+alias shtop='sudo htop'                       # run `htop` with root rights
+alias grep='grep --color=auto'                # colorize `grep` output
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias less='less -R'
+alias g='git'
+
+alias rm='rm -i'                              # confirm removal
+alias cp='cp -i'                              # confirm copy
+alias mv='mv -i'                              # confirm move
+alias cal='gcal --starting-day=1'             # print simple calendar for current month
+alias weather='curl v2.wttr.in'               # print weather for current location (https://github.com/chubin/wttr.in)
+
+#Neovim
+alias txt='nvim Text'
+alias nm='mv Text'
