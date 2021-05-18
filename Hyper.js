c2b0c4471636e188d8baa7b8c7620fe027286e48
Mans-from-the-ends <anton.gustavsson1999@icloud.com>
2021-05-18 19:13:57 +0200

    hyper

--- Hyper.js
+++ Hyper.js
@@ -1,20 +1,21 @@
 module.exports = {
-  	plugins: [
-        "hyper-font-ligatures",
-        "hyper-one-dark",
-        "hyper-search",
-        "hyper-pane",
-        "hypercwd",
-        "hyper-active-tab",
-        "hyperpower"
-    ],	
-  	config: {
-	shell: 'C:\\Windows\\System32\\bash.exe',
-	activeTab: '🚀',
-	webGLRenderer: false,
-	fontSize: 16,
-        fontFamily: '"SpaceMono NF", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-	  // rest of the config
+        plugins: [
+          "hyper-font-ligatures",
+          "hyper-one-dark",
+          "hyper-search",
+          "hyper-pane",
+          "hypercwd",
+          "hyper-active-tab",
+          "hyperline",
+          "hyperpower"
+        ],
+        config: {
+        activeTab: '🚀',
+        webGLRenderer: false,
+        fontSize: 16,
+        fontFamily: '"Space Mono For Powerline", monospace',
+          // rest of the config
   }
   // rest of the file
 }
+
